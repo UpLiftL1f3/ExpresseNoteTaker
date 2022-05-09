9c634a05ad1c60ab1820decb807f347633a447e1
KnightNight <gilbenvenitz18@gmail.com>
2022-05-09 16:32:32 -0500

    minor

--- server.js
+++ server.js
@@ -18,11 +18,11 @@ app.use(express.static('public'));
 
 //Get Route for homepage
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 //Get Route for NotesPage
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '/public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // app.get('/api/notes', (req, res) => {
